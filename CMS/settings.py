b5a607315fbcdcc1a39dca80b9695fb1582d5a6a
Taomercy <taomercy@qq.com>
2020-08-06 17:21:04 +0800

    revert mysql setting

--- CMS/settings.py
+++ CMS/settings.py
@@ -79,18 +79,18 @@ WSGI_APPLICATION = 'CMS.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': 'db',
-        'PORT': '3306',
-        'NAME': 'cms_database',
-        'USER': 'root',
-        'PASSWORD': 'root'
-    }
     # 'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'HOST': 'db',
+    #     'PORT': '3306',
+    #     'NAME': 'cms_database',
+    #     'USER': 'root',
+    #     'PASSWORD': 'root'
     # }
+    'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
