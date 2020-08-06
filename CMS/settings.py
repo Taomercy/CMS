bc2aa1dd4d9fe3f5e0f17c15422538cb4d16f8ed
Taomercy <taomercy@qq.com>
2020-08-06 16:42:27 +0800

    mysql setting

--- CMS/settings.py
+++ CMS/settings.py
@@ -80,9 +80,17 @@ WSGI_APPLICATION = 'CMS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'NAME': 'cms_database',
+        'USER': 'root',
+        'PASSWORD': 'root'
     }
+    # 'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
